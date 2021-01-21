7e0f0e5d334c8e949ca51e59a8d1d411767254b1
Pyleks <aleksander.andersen@outlook.ie>
2021-01-21 00:03:48 +0000

    Uploaded all images and made slight changes to categories

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    brand = models.CharField(max_length=254)
+    Software = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
