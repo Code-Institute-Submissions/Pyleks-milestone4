8272b9658f18d0e372f2de2c6960f689c5ae5bea
Pyleks <aleksander.andersen@outlook.ie>
2021-01-23 14:38:35 +0000

    Corrected view.py with messages

--- bag/views.py
+++ bag/views.py
@@ -29,11 +29,9 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] = quantity
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added {product.name} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request, f'Added {product.name} to your bag')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
