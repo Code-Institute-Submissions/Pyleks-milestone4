5a45df89b87d91093443dbd4e339a3df357cfa5f
Pyleks <aleksander.andersen@outlook.ie>
2021-01-24 13:37:09 +0000

    Corrected spelling in crispy_forms fields to field

--- Alex_Photography/settings.py
+++ Alex_Photography/settings.py
@@ -81,8 +81,8 @@ TEMPLATES = [
                 'bag.context.bag_contents',
             ],
             'builtins': [
-                'crispy_forms.templatetags.crispy_forms_tags'
-                'crispy_forms.templatetags.crispy_forms_fields'
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
 
             ]
         },
