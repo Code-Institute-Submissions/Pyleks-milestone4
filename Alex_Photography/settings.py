be9f19d41fb5b6d71018956a38862ea2cc884ae6
Pyleks <aleksander.andersen@outlook.ie>
2021-01-30 15:51:03 +0000

    Added heroku and localhost to allowed host

--- Alex_Photography/settings.py
+++ Alex_Photography/settings.py
@@ -28,7 +28,7 @@ DB = config('DATABASE_url')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alex-digital-art.herokuapp.com', 'localhost']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Alex_Photography.wsgi:application
