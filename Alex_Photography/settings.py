52ad80d3fd94c9a5e3493deecdbfa2834284f764
Pyleks <aleksander.andersen@outlook.ie>
2021-01-31 14:06:44 +0000

    Added environmental variables for storage_bucket and region name

--- Alex_Photography/settings.py
+++ Alex_Photography/settings.py
@@ -196,8 +196,8 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Settings
-    AWS_STORAGE_BUCKET_NAME = 'alex-digital-art'
-    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('REGION_NAME')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
