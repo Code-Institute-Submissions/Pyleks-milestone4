75286114139bd60fd718a64318606b5270770ac5
Pyleks <aleksander.andersen@outlook.ie>
2021-01-31 12:49:36 +0000

    Adding if statement to the databases

--- Alex_Photography/settings.py
+++ Alex_Photography/settings.py
@@ -132,22 +132,21 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'Alex_Photography.wsgi.application'
 
-DATABASES = {
-            'default': dj_database_url.parse(DATABASE)
-}
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# if 'DEBUG_STATUS' == "0":
-# DATABASES = {
-#     'default': dj_database_url.parse(DATABASE)
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-# else:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 
