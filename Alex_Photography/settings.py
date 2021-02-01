35c3875331e9d73fc9e3f30435ac9de2ad0358ed
Pyleks <aleksander.andersen@outlook.ie>
2021-02-01 12:44:13 +0000

    Added new CSS structure

--- Alex_Photography/settings.py
+++ Alex_Photography/settings.py
@@ -132,6 +132,7 @@ WSGI_APPLICATION = 'Alex_Photography.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
