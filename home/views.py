07f303721ee0350f5e321ba80a4911b09f569a52
Pyleks <aleksander.andersen@outlook.ie>
2021-02-02 15:07:51 +0000

    updated grid to work at correct breakpoints

--- home/views.py
+++ home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """A view to return the index paage """
 
     return render(request, 'home/index.html')
+
