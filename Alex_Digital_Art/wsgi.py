79df3c5c317eb3cfcd07de52a50a446b0b8f7966
Pyleks <aleksander.andersen@outlook.ie>
2021-02-05 05:55:51 +0000

    Renamed to correct project name

--- Alex_Digital_Art/wsgi.py
+++ Alex_Digital_Art/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alex_Photography.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alex_Digital_Art.settings')
 
 application = get_wsgi_application()
