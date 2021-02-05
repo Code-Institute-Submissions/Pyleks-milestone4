baa0482aaf1dcd5f1c9c5db1b05943a6df54e6eb
Pyleks <aleksander.andersen@outlook.ie>
2021-02-05 00:35:36 +0000

    Added comment for urls to profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# Required for accessing order history with correct order number.
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
